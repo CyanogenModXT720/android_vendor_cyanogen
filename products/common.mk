93beffa61731b91dd0e13f42306fedd99afd5711
peshovec <jlesev@gmail.com>
2011-10-25 10:23:07 +0300

    avoid RomManager.apk

--- products/common.mk
+++ products/common.mk
@@ -86,8 +86,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 #    vendor/cyanogen/prebuilt/common/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd
 
-PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/proprietary/RomManager.apk:system/app/RomManager.apk \
+#PRODUCT_COPY_FILES +=  \
+#    vendor/cyanogen/proprietary/RomManager.apk:system/app/RomManager.apk \
+#we dont need the RomManager
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
