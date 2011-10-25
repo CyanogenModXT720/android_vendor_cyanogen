1520de5f8b856a0e8c79c2208dc657f3edffb613
Mioze7Ae <Mioze7Ae@gmail.com>
2011-10-25 19:54:03 -0400

    Follow examples of sholes,shadow,jordan
    Change-Id: Ie9471039956bfef7b5d706b97aea34f591fee02a

--- products/cyanogen_sholest.mk
+++ products/cyanogen_sholest.mk
@@ -1,18 +1,11 @@
-# Inherit AOSP device configuration for passion.
+# Inherit AOSP device configuration.
 $(call inherit-product, device/motorola/sholest/sholest.mk)
 
+# Inherit common GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
-
-# Include GSM stuff
-#$(call inherit-product, vendor/cyanogen/products/gsm.mk)
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
-
-# Include the Flashlight/Torch
-PRODUCT_PACKAGES += Torch
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 #
 # Setup device specific product configuration.
@@ -23,25 +16,25 @@ PRODUCT_DEVICE := sholest
 PRODUCT_MODEL := Milestone XT720
 PRODUCT_MANUFACTURER := Motorola
 
-#
-# Set ro.modversion
-#
-ifdef CYANOGEN_NIGHTLY
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7-$(shell date +%d%n%Y)-NIGHTLY-MilestoneXT720
-else
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7.1.0-MilestoneXT720-beta-$(shell date +%d%m%Y) 
-endif
+# Release name and versioning
+PRODUCT_RELEASE_NAME := MilestoneXT720
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
 
-PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=R.U.R.1920 BUILD_DISPLAY_ID=R.U.R.1920 PRODUCT_NAME=sholest TARGET_DEVICE=sholest PRODUCT_BRAND=motorola   USER=fjfalcon
+PRODUCT_BUILD_PROP_OVERRIDES := \
+BUILD_ID=R.U.R.1920 \
+BUILD_DISPLAY_ID=R.U.R.1920 \
+PRODUCT_NAME=sholest \
+TARGET_DEVICE=sholest \
+PRODUCT_BRAND=motorola \
+USER=CyanogenModXT720
 
-# PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-umts_sholes.map
-
-# Enable Windows Media
-WITH_WINDOWS_MEDIA := true
-
-PRODUCT_COPY_FILES +=  \
+PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/sholest
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/cyanogen/overlay/sholest
+
+# Add the Torch app
+PRODUCT_PACKAGES += \
+    Torch
